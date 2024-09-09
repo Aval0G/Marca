1119a6168c722152793d82a8fcbf98a384ceae06
Aval0G <avaldez0@ucol.mx>
2024-09-09 11:19:17 -0600

    cards with v-for

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,42 @@
 <script setup>
 import { IHeader, IFooter, ISectionMultiCard, ITest, IHeaderNav } from './components'
 import { VApp } from 'vuetify/lib/components/index.mjs'
+
+const noticias = {
+  cards: [
+    {
+      src: 'https://cdn.vuetifyjs.com/images/cards/desert.jpg',
+      title: 'The Best Desert',
+      subtitle: 'The best desert in the world',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse in urna et felis.'
+    },
+    {
+      src: 'https://cdn.vuetifyjs.com/images/cards/docks.jpg',
+      title: 'The Best Docks',
+      subtitle: 'The best docks in the world',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse in urna et felis.'
+    },
+    {
+      src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
+      title: 'The Best Road',
+      subtitle: 'The best road in the world',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse in urna et felis.'
+    },
+    {
+      src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg',
+      title: 'The Best Plane',
+      subtitle: 'The best plane in the world',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse in urna et felis.'
+    }
+  ]
+}
 </script>
 
 <template>
   <v-app>
     <IHeader />
     <IHeaderNav />
-    <ISectionMultiCard />
+    <ISectionMultiCard :cards="noticias.cards" />
     <main></main>
     <ITest />
     <IFooter />
