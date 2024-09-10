4e64d0130c511a15b3f163ce30b7f37c4c6a671b
Avaldez0 <avaldez0@ucol.mx>
2024-09-09 20:21:16 -0600

    NewsTicker

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
-import './assets/global.scss'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import { createVuetify } from 'vuetify/lib/framework.mjs'
-// main.js o main.ts
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/_styles.scss'
-const vuetify = createVuetify()
-// vuetify configuration
-const app = createApp(App)
-app.use(vuetify).mount('#app')
+import { createVuetify } from 'vuetify';
+import '@mdi/font/css/materialdesignicons.css'; 
+import 'vuetify/styles'; 
+
+import './assets/global.scss';
+
+const vuetify = createVuetify();
+
+const app = createApp(App);
+app.use(vuetify).mount('#app');
 
 window.onbeforeunload = function () {
-  window.scrollTo(0, 0)
-}
+  window.scrollTo(0, 0);
+};
